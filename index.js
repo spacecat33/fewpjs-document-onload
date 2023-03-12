3eaccbad3aedc79b53415472e566991e2d64c835
Janan Akkad <jananakkad@hotmail.co.uk>
2023-03-12 16:22:15 +0000

    Completed:

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let text = document.getElementById("text");
+    text.textContent = "This is really cool!"; 
+});
+
+// code works if replace 'textContent' with 'innerText' or 'innerHTML'. 
